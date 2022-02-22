ff41dd9e591469ea974c2290bd552b12c2ac1037
HChai01 <88690720+HChai01@users.noreply.github.com>
2022-02-22 22:22:57 +0000

    Update GO_root_cellular_component.java

--- Source_code/src/Feature_encoding/GO_root_cellular_component.java
+++ Source_code/src/Feature_encoding/GO_root_cellular_component.java
@@ -33,7 +33,7 @@ import java.io.Writer;
  * F20:	GO:0097423	mitochondrion-associated_adherens_complex
  * F21:	GO:0099080	supramolecular_complex
  * 
- * Citation: Haiting Chai, Quan Gu, Joseph Hughes and David L. Robertson (202X), In silico prediction of HIV-1-host molecular interactions and their directionality, PLOS Computational Biology, XX(X): XXX-XXX, PMID: XXXXXXXX.
+ * Citation: Chai H, Gu Q, Hughes J, Robertson DL (2022) In silico prediction of HIV-1-host molecular interactions and their directionality. PLoS Comput Biol 18(2): e1009720. https://doi.org/10.1371/journal.pcbi.1009720.
  * 
  * */
 
